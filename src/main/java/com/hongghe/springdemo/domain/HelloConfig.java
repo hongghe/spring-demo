d7c140b4984eb9dfbf49736611bf19de28e2e9e9
hongghe <364930855@qq.com>
2018-07-11 14:10:16 +0800

    configuration.

---
+++ src/main/java/com/hongghe/springdemo/domain/HelloConfig.java
@@ -0,0 +1,10 @@
+package com.hongghe.springdemo.domain;
+
+import lombok.Data;
+
+@Data
+public class HelloConfig {
+    private String username;
+    private Integer port;
+    private Long password;
+}
