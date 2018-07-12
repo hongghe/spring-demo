cab1946e96216b4c77e619b2c9195654d4a48fbb
hongghe <364930855@qq.com>
2018-07-12 10:49:28 +0800

    Adapter of interceptor and filter.

---
+++ src/main/java/com/hongghe/springdemo/filter/HelloFilter.java
@@ -0,0 +1,14 @@
+package com.hongghe.springdemo.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello filter.It is a filter of filter greeting message.
+ *
+ * @author hongghe
+ */
+public class HelloFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloFilter.class);
+}
---
+++ src/main/java/com/hongghe/springdemo/interceptor/HelloInterceptor.java
@@ -0,0 +1,22 @@
+package com.hongghe.springdemo.interceptor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello interceptor. It is intercept greeting message.
+ *
+ * @author hongghe
+ */
+public class HelloInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloInterceptor.class);
+
+    public void pre() throws Exception {
+        try {
+
+        } catch (Exception e) {
+            //
+        }
+    }
+}
